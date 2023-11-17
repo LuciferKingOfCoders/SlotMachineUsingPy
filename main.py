e62a5f869b6e863d191df3c9249e9da4072f84a7
SREENADTH KRISHNA <134028402+LuciferKingOfCoders@users.noreply.github.com>
2023-11-16 22:50:09 -0500

    symbols function created

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
+import random
+
+
+
 MAX_LINES =3        # global var
 MAX_BET = 100
 MIN_BET = 1
+
+ROW =3
+COLS =3
+
+symbol_count ={
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols =[]
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+            
+    columns = [[], [], []]
+    for _ in range(cols):
+        column =[]
+        current_symbols =all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+            
+        columns.append(column)
+
+
+
+
 
 
 def deposit():
@@ -47,12 +82,19 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet  > balance:
+            print(f"You do not have enough to bet that amount, your current balance is ${balance} ")
+        else:
+            break
+
     print(f"You are betting ${bet} on ${lines} lines. Total bet is equal to: ${total_bet} ")
 
 
-    print(balance, lines)
+
 
 
 main()
