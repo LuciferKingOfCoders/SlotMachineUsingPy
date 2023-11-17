b582b5f62545f29bd88fbe19d5da2b7ecd5c3413
SREENADTH KRISHNA <134028402+LuciferKingOfCoders@users.noreply.github.com>
2023-11-16 19:21:10 -0500

    created primary def

---
+++ main.py
@@ -0,0 +1,36 @@
+MAX_LINES =3        # global var
+
+def deposit():
+    while True:
+        amount = input ("How much would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amouont must be greater than 0.")
+        else:
+            print("Pls enter a number: ")
+    return amount
+
+def get_number_of_lines():
+     while True:
+        lines = input ("Enter the number of lines to bet on (1-" +str(MAX_LINES) + ")?")
+        if lines.isdigit():        # Check if input is a digit
+            lines = int(lines)
+            if 1<= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Pls enter a number: ")
+        return lines
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+
+main()
